2bbb8e991d6700412660fe7bdf2644fb7dbfc974
Jonibek <akmuratovj@gmail.com>
2025-10-14 18:36:10 +0500

    Commit

--- src/main/java/com/example/demoproject/dto/main/product/ProductCreateDTO.java
+++ src/main/java/com/example/demoproject/dto/main/product/ProductCreateDTO.java
@@ -3,4 +3,14 @@ package com.example.demoproject.dto.main.product;
 import com.example.demoproject.dto.DTO;
 
 public class ProductCreateDTO  implements DTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private Long categoryId;
 }
--- src/main/java/com/example/demoproject/dto/main/product/ProductDTO.java
+++ src/main/java/com/example/demoproject/dto/main/product/ProductDTO.java
@@ -1,6 +1,17 @@
 package com.example.demoproject.dto.main.product;
 
 import com.example.demoproject.dto.DTO;
+import com.example.demoproject.dto.main.category.CategoryDTO;
 
 public class ProductDTO  implements DTO {
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private CategoryDTO category;
 }
