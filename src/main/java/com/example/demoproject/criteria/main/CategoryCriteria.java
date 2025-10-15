75e668c171ae04f3261e7225c72fb603befa1004
Jonibek <akmuratovj@gmail.com>
2025-10-15 16:00:45 +0500

    Commit

--- src/main/java/com/example/demoproject/criteria/main/CategoryCriteria.java
+++ src/main/java/com/example/demoproject/criteria/main/CategoryCriteria.java
@@ -11,5 +11,6 @@ import lombok.Setter;
 @NoArgsConstructor
 public class CategoryCriteria extends GenericCriteria {
 
+    private String name;
 
 }
--- src/main/java/com/example/demoproject/criteria/main/ProductCriteria.java
+++ src/main/java/com/example/demoproject/criteria/main/ProductCriteria.java
@@ -1,6 +1,7 @@
 package com.example.demoproject.criteria.main;
 
 import com.example.demoproject.criteria.GenericCriteria;
+import com.example.demoproject.dto.main.category.CategoryDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -10,6 +11,12 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class ProductCriteria extends GenericCriteria {
+
+    private String name;
+
+    private Double price;
+
+    private Long categoryId;
 
 
 }
