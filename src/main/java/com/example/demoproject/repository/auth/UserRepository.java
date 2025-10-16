1cf4452ca2dad3352ea5cd0d350afaf50ee06a7c
Jonibek <akmuratovj@gmail.com>
2025-10-16 09:18:25 +0500

    Commit

--- src/main/java/com/example/demoproject/repository/auth/UserRepository.java
+++ src/main/java/com/example/demoproject/repository/auth/UserRepository.java
@@ -1,17 +1,11 @@
 package com.example.demoproject.repository.auth;
 
-import com.example.demoproject.criteria.auth.UserCriteria;
 import com.example.demoproject.entity.auth.User;
-import com.example.demoproject.repository.GenericCrudRepository;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
 
     Optional<User> findByUsername(String username);
 
